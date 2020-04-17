cbd304bd21fddbb82a3bad6bf5af33ef76db479a
Yana <yanacommit@wizwar.net>
2020-04-17 15:58:39 -0700

    Implement grid generator system without listener.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,8 +3,35 @@ import "../../css/global.css"
 import "../../css/type.css"
 import "./layout.css"
 
+
+// By default 4.
+// If 768 and above, 8
+// If 1024 and above, 12.
+
+function buildGrid(numRows) {
+  var numCols;
+  if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)").matches) {
+    numCols = 8;
+  } else if (window.matchMedia("(min-width: 102 4px)").matches) {
+    numCols = 12
+  } else {
+    numCols = 4;
+  }
+
+  console.log(numCols);
+  var sqArray = []
+  for (var i = 0; i < numRows*numCols; i++) {
+    sqArray.push(<div id="js-square" className="square"></div>);
+  }
+  return sqArray;
+
+
+}
+
 export default props => (
   <>
-       
+    <div id="js-rowContainer" className="row-container">
+      {buildGrid(3)}
+    </div>
   </>
 );  
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const Index = () => (
   <>
     <SEO title="Pixelation" />
     <Header />
+    <Layout />
   </>
 )
 
