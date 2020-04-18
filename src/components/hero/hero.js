f75153321bafb2d4cb5d34537c8701f6cb4df9cd
Yana <yanacommit@wizwar.net>
2020-04-17 23:18:32 -0700

    Complete responsive hero.

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,6 +4,7 @@ import "../../css/global.css"
 import "../../css/reset.css"
 import "../../css/type.css"
 import Layout from "../layout/layout"
+import smiley from "../../images/smileyFace.svg" 
 // import Pixelator from "../pixelator/pixelator"
 import "./hero.css"
 
@@ -12,7 +13,10 @@ export default props => (
     <Layout rows="8">
         <main className="hero">
             <p className="hero__brow">MICA GD BFA 2020 Degree Show</p>
-            <h1 className="hero__lead">Pixels</h1>
+            <header className="hero__lead--wrapper">
+                <h1 className="hero__lead">Pixels</h1>
+                <img className="hero__smiley" src={smiley} alt=""/>
+            </header>
             <h4 className="hero__paragraph">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh</h4>
         </main>
     </Layout>
