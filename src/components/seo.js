2a5de28d77ad27cf2b8aa1623f48adb4d37f9c58
Yana <yanacommit@wizwar.net>
2020-04-18 00:30:54 -0700

    Remove example link from seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,6 +6,5 @@ export default props =>  (
   <Helmet titleTemplate="%s · MICA GD 2020">
     <meta charSet="utf-8" />
     <title>{props.title}</title>
-    <link rel="canonical" href="http://mysite.com/example" />
   </Helmet>
 )
