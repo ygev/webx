4e9b0b78a17b7797122aa674041ce6a6e5187612
Yana <yanacommit@wizwar.net>
2020-04-18 19:20:07 -0700

    Add a scrolling mobile alphabet.

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,13 +1,33 @@
 import React from "react"
 import { Link } from "gatsby"
-import Slide from 'react-reveal/Slide';
+import Pixelator from "../pixelator/pixelator"
 import "../../css/global.css"
 import "../../css/reset.css"
 import "../../css/type.css"
 import "./search.css"
+import randomize from "../../images/randomize.svg"
 
 export default props => (
   <>
-    <section className="search__title"></section>
+    <section className="search__title">
+        <h3 className="search__title--txt"><Pixelator content="Search by Last Name"/></h3>
+    </section>
+    <div className="glossary--scroller">
+    <section className="glossary">
+        <div className="glossary__item glossary__item--active glossary__randomize">
+            <img src={randomize} alt="" className="glossay__randomize--img"/>
+        </div>
+        <p className="glossary__item glossary__letter">A</p>
+        <p className="glossary__item glossary__letter glossary__letter--ghost">B</p>
+        <p className="glossary__item glossary__letter">C</p>
+        <p className="glossary__item glossary__letter">D</p>
+        <p className="glossary__item glossary__letter">E</p>
+        <p className="glossary__item glossary__letter">F</p>
+        <p className="glossary__item glossary__letter">G</p>
+        <p className="glossary__item glossary__letter">H</p>
+        <p className="glossary__item glossary__letter">I</p>
+        <p className="glossary__item glossary__letter">J</p>
+    </section>
+    </div>
   </>
 );  
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import SEO from "../components/seo"
 import Header from "../components/header/header"
 import Hero from "../components/hero/hero"
+import Search from "../components/search/search"
 import Footer from "../components/footer/footer"
 
 const Index = () => (
@@ -9,6 +10,7 @@ const Index = () => (
     <SEO title="Pixelation" />
     <Header />
     <Hero/>
+    <Search/>
     <Footer/>
   </>
 )
