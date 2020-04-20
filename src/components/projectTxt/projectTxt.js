d7cb96f9018487a29b1f1420e651610040dd8669
Yana <yanacommit@wizwar.net>
2020-04-19 23:16:10 -0700

    Complete projectGallery the lazy way.

--- src/components/projectTxt/projectTxt.js
+++ src/components/projectTxt/projectTxt.js
@@ -9,7 +9,7 @@ import "./projectTxt.css"
 
 export default props => (
     <>
-        <Layout rows={[3, 3, 3]}>
+        <Layout rows={[3, 4, 3]}>
           <section className="projectTxt">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat elit, consequat habitasse convallis. Tempus vitae ac, nulla sed massa, tempor. Velit maecenas tortor quis consequat elementum commodo et. Nulla egestas non sagittis, aliquam. Mauris, tincidunt nulla ut arcu egestas suspendisse ipsum, ornare mattis. Placerat pharetra, viverra habitant fringilla nec odio maecenas lectus faucibus. Facilisis sem mauris, ullamcorper senectus. Sed quisque pulvinar id tortor, euismod. Sapien arcu nisi tristique pharetra in. Pretium posuere sem faucibus adipiscing enim nulla velit se ipsum, ornare  enim, mae.</p>
           </section>
