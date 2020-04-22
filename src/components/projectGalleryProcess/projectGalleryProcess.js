4caac2a78802f9694dfdb6b78edbf10baeead203
Yana <yanacommit@wizwar.net>
2020-04-21 22:17:19 -0700

    Make any variable image work in galleries.

--- src/components/projectGalleryProcess/projectGalleryProcess.js
+++ src/components/projectGalleryProcess/projectGalleryProcess.js
@@ -6,9 +6,10 @@ import "../../css/global.css"
 import "../../css/reset.css"
 import "../../css/type.css"
 import "./projectGalleryProcess.css"
-import imgA from "../../images/_sample-data/projects/final-1.png"
-import imgB from "../../images/_sample-data/projects/final-2.png"
-import imgC from "../../images/_sample-data/projects/final-3.png"
+import imgA from "../../images/_sample-data/projects/process-1.png"
+import imgB from "../../images/_sample-data/projects/process-2.png"
+import imgC from "../../images/_sample-data/projects/process-3.png"
+import imgD from "../../images/_sample-data/projects/process-4.png"
 import Fade from 'react-reveal/Fade';
 
 export default props => (
@@ -18,6 +19,7 @@ export default props => (
               <img src={imgA} alt="" className="projectGalleryProcess__item"/>
               <img src={imgB} alt="" className="projectGalleryProcess__item"/>
               <img src={imgC} alt="" className="projectGalleryProcess__item"/>
+              <img src={imgD} alt="" className="projectGalleryProcess__item"/>
           </section>
     </Fade>
     </>
--- src/components/projectPreFooter/projectPreFooter.js
+++ src/components/projectPreFooter/projectPreFooter.js
@@ -14,7 +14,9 @@ export default props => (
         <Layout rows={[2, 2, 2]}>
                 <section className="projectPreFooter__back--wrapper">
                     <Link to="/">
+                <section className="projectPreFooter__back--wrapper">
                         <img src={arrowLeft} alt="Go Back to the Home Page" className="projectPreFooter__back"/>
+                </section>
                     </Link>
                 </section>
                 <section className="projectPreFooter__random--wrapper">
