483b30c9c9b64c7dbe16151373da665215b076c4
Yana <yanacommit@wizwar.net>
2020-04-21 21:13:27 -0700

    Complete desktop html/css for search.

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -6,7 +6,8 @@ import "../../css/reset.css"
 import "../../css/type.css"
 import "./search.css"
 import randomize from "../../images/randomize.svg"
-import profile from "../../images/_sample-data/profiles/yana.png"
+import profile from "../../images/_sample-data/profiles/tony.png"
+import profilePlaceholder from "../../images/_sample-data/profiles/profile--placeholder.svg"
 import previewA from "../../images/_sample-data/projects--previews/mood.png"
 import previewB from "../../images/_sample-data/projects--previews/blur.png"
 import arrowRightTeal from "../../images/arrowRight--teal.svg"
@@ -19,7 +20,7 @@ export default props => (
         </div>
         <div className="glossary--scroller">
             <section className="glossary">
-                <div className="glossary__item glossary__item--active glossary__randomize">
+                <div aria-label="Randomize Projects" className="glossary__item glossary__item--active glossary__randomize">
                     <img src={randomize} alt="" className="glossay__randomize--img"/>
                 </div>
                 <p className="glossary__item glossary__letter">A</p>
@@ -50,23 +51,35 @@ export default props => (
             </section>
         </div>
     </section>
-    <section className="photo__grid">
-        <img src={previewA} alt="" className="photo__grid--img"/>
-        <img src={previewB} alt="" className="photo__grid--img"/>
-        <img src={previewB} alt="" className="photo__grid--img"/>
-        <img src={previewA} alt="" className="photo__grid--img"/>
-        <img src={previewA} alt="" className="photo__grid--img"/>
-        <img src={previewB} alt="" className="photo__grid--img"/>
-        <img src={previewB} alt="" className="photo__grid--img"/>
-        <img src={previewA} alt="" className="photo__grid--img"/>
-        <img src={previewB} alt="" className="photo__grid--img"/>
-        <img src={previewA} alt="" className="photo__grid--img"/>
-        <img src={previewB} alt="" className="photo__grid--img"/>
-        <img src={previewA} alt="" className="photo__grid--img"/>
-    </section>
+    <div className="search__info--wrapper">
+        <div className="search__desktop--wrapper">
+            <section className="search__profile">
+                <div className="search__profile--frame"></div>
+                <img src={profilePlaceholder} alt="" className="search__profile--img"/>
+            </section>
+            <section className="search__name">
+                <h3 className="cta__name"><Pixelator content="MIA KARLIN-CAPELLO"/></h3>
+                <h4 className="cta__project">My Leetl Project</h4>
+            </section>
+        </div>
+        <section className="photo__grid">
+            <img src={previewA} alt="" className="photo__grid--img"/>
+            <img src={previewB} alt="" className="photo__grid--img"/>
+            <img src={previewB} alt="" className="photo__grid--img"/>
+            <img src={previewA} alt="" className="photo__grid--img"/>
+            <img src={previewA} alt="" className="photo__grid--img"/>
+            <img src={previewB} alt="" className="photo__grid--img"/>
+            <img src={previewB} alt="" className="photo__grid--img"/>
+            <img src={previewA} alt="" className="photo__grid--img"/>
+            <img src={previewB} alt="" className="photo__grid--img"/>
+            <img src={previewA} alt="" className="photo__grid--img"/>
+            <img src={previewB} alt="" className="photo__grid--img"/>
+            <img src={previewA} alt="" className="photo__grid--img"/>
+        </section>  
+    </div>
     <section className="cta">
         <div className="cta__txt">
-            <h3 className="cta__name"><Pixelator content="Amanda Yeh"/></h3>
+            <h3 className="cta__name"><Pixelator content="MIA KARLIN-CAPELLO"/></h3>
             <h4 className="cta__project">My Leetl Project</h4>
         </div>
         <img src={arrowRightTeal} alt="" className="cta__arrow"/>
