ff7fdec6aed15eedb888cce1e23948b1d0dff3f6
Yana <yanacommit@wizwar.net>
2020-04-22 22:13:40 -0700

    Add fonts into static and move the folder to css.

---
+++ src/components/fonts.js
@@ -0,0 +1,43 @@
+import OnePixelEOT from "../fonts/OnePixel/OnePixelRegular.eot";
+import OnePixelWOFF2 from "../fonts/OnePixel/OnePixelRegular.woff2";
+import OnePixelWOFF from "../fonts/OnePixel/OnePixelRegular.woff";
+import OnePixelTTF from "../fonts/OnePixel/OnePixelRegular.ttf";
+
+import SuisseBoldEOT from "../fonts/Suisse/SuisseIntl-Bold.eot";
+import SuisseBoldWOFF2 from "../fonts/Suisse/SuisseIntl-Bold.woff2";
+import SuisseBoldWOFF from "../fonts/Suisse/SuisseIntl-Bold.woff2";
+import SuisseBoldTTF from "../fonts/Suisse/SuisseIntl-Bold.ttf";
+
+import SuisseMediumEOT from "../fonts/Suisse/SuisseIntl-Medium.eot";
+import SuisseMediumWOFF2 from "../fonts/Suisse/SuisseIntl-Medium.woff2";
+import SuisseMediumWOFF from "../fonts/Suisse/SuisseIntl-Medium.woff";
+import SuisseMediumTTF from "../fonts/Suisse/SuisseIntl-Medium.ttf";
+
+import SuisseRegularEOT from "../fonts/Suisse/SuisseIntl-Regular.eot";
+import SuisseRegularWOFF2 from "../fonts/Suisse/SuisseIntl-Regular.woff2";
+import SuisseRegularWOFF from "../fonts/Suisse/SuisseIntl-Regular.woff";
+import SuisseRegularTTF from "../fonts/Suisse/SuisseIntl-Regular.ttf";
+
+
+export {
+    OnePixelEOT,
+    OnePixelWOFF2,
+    OnePixelWOFF,
+    OnePixelTTF,
+
+    SuisseBoldEOT,
+    SuisseBoldWOFF2,
+    SuisseBoldWOFF,
+    SuisseBoldTTF,
+
+    SuisseMediumEOT,
+    SuisseMediumWOFF2,
+    SuisseMediumWOFF,
+    SuisseMediumTTF,
+
+    SuisseRegularEOT,
+    SuisseRegularWOFF2,
+    SuisseRegularWOFF,
+    SuisseRegularTTF
+
+};
