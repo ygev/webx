86783bb3de2130f7a58626e683856897693fc269
Amanda Yeh <amandayeh123@gmail.com>
2020-04-29 12:49:18 -0400

    added link to project perview

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -20,43 +20,44 @@ class Search extends React.Component {
     this.isDown = false;
 
     this.state = {
+      linkProject: props.linkProject,
       studentName: props.studentName,
       projectName: props.projectName,
-      profilePicture: props.profilePicture
+      profilePicture: props.profilePicture,
     };
   }
 
-  sliderMouseDown(e){
+  sliderMouseDown(e) {
     if (typeof document !== `undefined`) {
       this.isDown = true;
-      var slider = document.getElementsByClassName('glossary--scroller')[0];
-      slider.classList.add('active');
+      var slider = document.getElementsByClassName("glossary--scroller")[0];
+      slider.classList.add("active");
       this.startX = e.pageX - slider.offsetLeft;
       this.scrollLeft = slider.scrollLeft;
     }
   }
 
-  sliderMouseLeave(){
+  sliderMouseLeave() {
     if (typeof document !== `undefined`) {
       this.isDown = false;
-      var slider = document.getElementsByClassName('glossary--scroller')[0];
-      slider.classList.remove('active');
+      var slider = document.getElementsByClassName("glossary--scroller")[0];
+      slider.classList.remove("active");
     }
   }
 
-  sliderMouseUp(){
+  sliderMouseUp() {
     if (typeof document !== `undefined`) {
       this.isDown = false;
-      var slider = document.getElementsByClassName('glossary--scroller')[0];
-      slider.classList.remove('active');
+      var slider = document.getElementsByClassName("glossary--scroller")[0];
+      slider.classList.remove("active");
     }
   }
 
-  sliderMouseMove(e){
+  sliderMouseMove(e) {
     if (typeof document !== `undefined`) {
-      if(!this.isDown) return;
+      if (!this.isDown) return;
       e.preventDefault();
-      var slider = document.getElementsByClassName('glossary--scroller')[0];
+      var slider = document.getElementsByClassName("glossary--scroller")[0];
       const x = e.pageX - slider.offsetLeft;
       const walk = (x - this.startX) * 1;
       slider.scrollLeft = this.scrollLeft - walk;
@@ -65,30 +66,51 @@ class Search extends React.Component {
   }
 
   handleProjectHover(project) {
-    this.setState({studentName: project.whatIsYourFirstName + ' ' + project.whatIsYourLastName});
-    this.setState({projectName: project.whatIsYourProjectName});
-    this.setState({profilePicture: require("../../images/_sample-data/projects/" + project.emailAddress + "/WebCamPicture/headshot.png")});
+    this.setState({
+      linkProject:
+        "/" + project.whatIsYourFirstName + project.whatIsYourLastName,
+    });
+    this.setState({
+      studentName:
+        project.whatIsYourFirstName + " " + project.whatIsYourLastName,
+    });
+    this.setState({ projectName: project.whatIsYourProjectName });
+    this.setState({
+      profilePicture: require("../../images/_sample-data/projects/" +
+        project.emailAddress +
+        "/WebCamPicture/headshot.png"),
+    });
   }
 
   handleLetterHover(argLetter) {
-    let argLetterItems = document.getElementsByClassName('letter-' + argLetter);
+    let argLetterItems = document.getElementsByClassName("letter-" + argLetter);
 
     if (argLetterItems.length > 0) {
       for (var i = 0; i < 26; i++) {
-        let currentLetter = (i+10).toString(36);
-        let currentLetterItems = document.getElementsByClassName('letter-' + currentLetter);
-        
+        let currentLetter = (i + 10).toString(36);
+        let currentLetterItems = document.getElementsByClassName(
+          "letter-" + currentLetter
+        );
+
         for (let item of currentLetterItems) {
           if (currentLetter == argLetter) {
-            document.getElementsByClassName("glossary__randomize")[0].classList.remove("glossary__item--active");
-            document.getElementsByClassName("glossary__randomize--img")[0].src = require("../../images/randomize--black.svg")
-            document.getElementsByClassName("glossary__letter")[i].classList.add("glossary__item--active");
+            document
+              .getElementsByClassName("glossary__randomize")[0]
+              .classList.remove("glossary__item--active");
+            document.getElementsByClassName(
+              "glossary__randomize--img"
+            )[0].src = require("../../images/randomize--black.svg");
+            document
+              .getElementsByClassName("glossary__letter")
+              [i].classList.add("glossary__item--active");
             item.firstChild.style.height = "";
             item.firstChild.style.width = "";
           } else {
             item.firstChild.style.height = "0";
             item.firstChild.style.width = "0";
-            document.getElementsByClassName("glossary__letter")[i].classList.remove("glossary__item--active");
+            document
+              .getElementsByClassName("glossary__letter")
+              [i].classList.remove("glossary__item--active");
           }
         }
       }
@@ -98,10 +120,14 @@ class Search extends React.Component {
   ungrayLetter() {
     if (typeof document !== `undefined`) {
       for (var i = 0; i < 26; i++) {
-        let currentLetter = (i+10).toString(36);
-        let currentLetterItems = document.getElementsByClassName('letter-' + currentLetter);
+        let currentLetter = (i + 10).toString(36);
+        let currentLetterItems = document.getElementsByClassName(
+          "letter-" + currentLetter
+        );
         if (currentLetterItems.length == 1) {
-          document.getElementsByClassName("glossary__letter")[i].classList.remove("glossary__letter--ghost");
+          document
+            .getElementsByClassName("glossary__letter")
+            [i].classList.remove("glossary__letter--ghost");
         }
       }
     }
@@ -110,12 +136,20 @@ class Search extends React.Component {
   randomizeProjects() {
     // for now, just show all letters... todo, actually randomize the projects
     for (var i = 0; i < 26; i++) {
-      let currentLetter = (i+10).toString(36);
-      let currentLetterItems = document.getElementsByClassName('letter-' + currentLetter);
+      let currentLetter = (i + 10).toString(36);
+      let currentLetterItems = document.getElementsByClassName(
+        "letter-" + currentLetter
+      );
 
-      document.getElementsByClassName("glossary__randomize")[0].classList.add("glossary__item--active");
-      document.getElementsByClassName("glossary__randomize--img")[0].src = require("../../images/randomize.svg")
-      document.getElementsByClassName("glossary__letter")[i].classList.remove("glossary__item--active");
+      document
+        .getElementsByClassName("glossary__randomize")[0]
+        .classList.add("glossary__item--active");
+      document.getElementsByClassName(
+        "glossary__randomize--img"
+      )[0].src = require("../../images/randomize.svg");
+      document
+        .getElementsByClassName("glossary__letter")
+        [i].classList.remove("glossary__item--active");
 
       for (let item of currentLetterItems) {
         item.firstChild.style.height = "";
@@ -126,12 +160,24 @@ class Search extends React.Component {
 
   generateLetters() {
     var letters = [];
-    for (var i = 0; i < 26; i++) { // iterate through the alphabet
-      let thisLetter = (i+10).toString(36); // thisLetter = a, b, c...
+    for (var i = 0; i < 26; i++) {
+      // iterate through the alphabet
+      let thisLetter = (i + 10).toString(36); // thisLetter = a, b, c...
       if (this.initialLetterState[thisLetter] == true) {
-        letters.push(<p className="glossary__item glossary__letter" onClick={() => this.handleLetterHover(thisLetter)}>{thisLetter}</p>);
+        letters.push(
+          <p
+            className="glossary__item glossary__letter"
+            onClick={() => this.handleLetterHover(thisLetter)}
+          >
+            {thisLetter}
+          </p>
+        );
       } else {
-        letters.push(<p className="glossary__item glossary__letter glossary__letter--ghost">{thisLetter}</p>);
+        letters.push(
+          <p className="glossary__item glossary__letter glossary__letter--ghost">
+            {thisLetter}
+          </p>
+        );
       }
     }
     return letters;
@@ -140,16 +186,14 @@ class Search extends React.Component {
   generateProjects() {
     let generatedProjects = Projects.map((project, index) => {
       // un-gray this student's letter
-      this.initialLetterState[project.whatIsYourLastName[0].toLowerCase()] = true;
-      
+      this.initialLetterState[
+        project.whatIsYourLastName[0].toLowerCase()
+      ] = true;
+
       // append this student to our generatedStudents array
       return (
         <Link
-          to={
-            "/" +
-            project.whatIsYourFirstName +
-            project.whatIsYourLastName
-          }
+          to={"/" + project.whatIsYourFirstName + project.whatIsYourLastName}
           className={"letter-" + project.whatIsYourLastName[0].toLowerCase()}
           onMouseEnter={() => this.handleProjectHover(project)} // use () => this.function() syntax to prevent losing 'this' context
         >
@@ -162,7 +206,7 @@ class Search extends React.Component {
           />
         </Link>
       );
-    })
+    });
     return generatedProjects;
   }
 
@@ -170,63 +214,73 @@ class Search extends React.Component {
     return (
       <div class="fucker__flipper">
         <Fade cascade duration={500}>
-        <section className="cta">
+          <section className="cta">
             <div className="cta__txt">
               <h3 className="cta__name">
-              <Pixelator content={this.state.studentName} />
+                <Pixelator content={this.state.studentName} />
               </h3>
               <h4 className="cta__project">{this.state.projectName}</h4>
             </div>
             <Link to="/project">
               <img src={arrowRightTeal} alt="" className="cta__arrow" />
             </Link>
-        </section>
-        <div className="search__info--wrapper">
-            <div className="search__desktop--wrapper">
-              <section className="search__profile">
-                <div className="search__profile--frame"></div>
-                <img
-                  src={this.state.profilePicture}
-                  alt=""
-                  className="search__profile--img"
-                  id="profile-pic"
-                />
-              </section>
-              <section className="search__name">
-                <h3 className="cta__name" id="studentName">
-                  <Pixelator content={this.state.studentName} />
-                </h3>
-                <h4 className="cta__project" id="projectName">
-                  {this.state.projectName}
-                </h4>
+          </section>
+          <Link className="search__link" to={this.state.linkProject}>
+            <div className="search__info--wrapper">
+              <div className="search__desktop--wrapper">
+                <section className="search__profile">
+                  <div className="search__profile--frame"></div>
+                  <img
+                    src={this.state.profilePicture}
+                    alt=""
+                    className="search__profile--img"
+                    id="profile-pic"
+                  />
+                </section>
+                <section className="search__name">
+                  <h3 className="cta__name" id="studentName">
+                    <Pixelator content={this.state.studentName} />
+                  </h3>
+                  <h4 className="cta__project" id="projectName">
+                    {this.state.projectName}
+                  </h4>
+                </section>
+              </div>
+              <section className="photo__grid">
+                {this.generateProjects()}
               </section>
             </div>
-            <section className="photo__grid">
-              {this.generateProjects()}
-            </section>
-        </div>
-         
-        <section className="searchglossary--wrapper">
-          <div className="search__title">
-            <h3 className="search__title--txt">
-              <Pixelator content="Search By Last Name" />
-            </h3>
-          </div>
-          <div className="glossary--scroller" 
-          onMouseDown={(e) => this.sliderMouseDown(e)}
-          onMouseLeave={(e) => this.sliderMouseLeave(e)}
-          onMouseUp={(e) => this.sliderMouseUp(e)}
-          onMouseMove={(e) => this.sliderMouseMove(e)}>
-            <section className="glossary">
-              <div onClick={this.randomizeProjects} aria-label="Randomize Projects" className="glossary__item glossary__item--active glossary__randomize">
-                <img src={randomize} alt="" className="glossary__randomize--img"/>
-              </div>
-              {this.generateLetters()}
-            </section>
-          </div>
-        </section>
+          </Link>
 
-          
+          <section className="searchglossary--wrapper">
+            <div className="search__title">
+              <h3 className="search__title--txt">
+                <Pixelator content="Search By Last Name" />
+              </h3>
+            </div>
+            <div
+              className="glossary--scroller"
+              onMouseDown={(e) => this.sliderMouseDown(e)}
+              onMouseLeave={(e) => this.sliderMouseLeave(e)}
+              onMouseUp={(e) => this.sliderMouseUp(e)}
+              onMouseMove={(e) => this.sliderMouseMove(e)}
+            >
+              <section className="glossary">
+                <div
+                  onClick={this.randomizeProjects}
+                  aria-label="Randomize Projects"
+                  className="glossary__item glossary__item--active glossary__randomize"
+                >
+                  <img
+                    src={randomize}
+                    alt=""
+                    className="glossary__randomize--img"
+                  />
+                </div>
+                {this.generateLetters()}
+              </section>
+            </div>
+          </section>
         </Fade>
       </div>
     );
