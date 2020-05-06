530f3a726ec497ef343fce3253dd089993205083
Yana <yanacommit@wizwar.net>
2020-05-06 09:46:41 -0700

    Apply pixelator to header.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Fade from 'react-reveal/Fade';
+// import Pixelator from "../pixelator/pixelator"
 import "../../css/global.css"
 import "../../css/reset.css"
 import "../../css/type.css"
@@ -11,10 +12,14 @@ export default props => (
     <Fade duration={1000}>
        <header className="header__wrapper">
            <Link to="/">
-               <h3 className="header__txt">Pixelation</h3>
+               <h3 className="header__txt">
+                <span className="header__txt--pixelate">P</span>ixelation
+              </h3>
            </Link>
            <Link to="/about">
-               <h3 className="header__txt">About</h3>
+               <h3 className="header__txt">
+                <span className="header__txt--pixelate">A</span>bout
+              </h3>
            </Link>
        </header>
     </Fade>
