6d553256bc167746be5ae6e776baf55977c94831
Yana <yanacommit@wizwar.net>
2020-05-07 22:38:04 -0700

    Add aria-labels to bastardized header links.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,12 +12,12 @@ export default props => (
     <Fade duration={1000}>
        <header className="header__wrapper">
            <Link to="/">
-               <h3 className="header__txt">
+               <h3 aria-label="Pixelation: Home Page" className="header__txt">
                 Pi<span className="header__txt--pixelate">X</span>elation
               </h3>
            </Link>
            <Link to="/about">
-               <h3 className="header__txt">
+               <h3 aria-label="About Page" className="header__txt">
                 <span className="header__txt--pixelate">A</span>bout
               </h3>
            </Link>
