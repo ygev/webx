eff0316c017a6d2d7ff9a72af2544c678927e854
Yana <yanacommit@wizwar.net>
2020-05-12 12:53:11 -0700

    Use semantic <nav> element intsead.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -16,9 +16,9 @@ export default props => (
                 Pi<span className="header__txt--pixelate">X</span>elation
               </h3>
            </Link>
-           <section aria-label="Mobile Navigation" className="link__minor--mobile">
+           <nav aria-label="Mobile Navigation" className="link__minor--mobile">
                <img src={navBar} alt="" className="link__minor--img"/>
-           </section>
+           </nav>
            <section className="link__minor">
               <Link to="/">
                   <h3 aria-label="All Projects" className="header__txt header__txt--minor">
